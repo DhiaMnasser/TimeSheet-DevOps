c7bebf7e2a6592785f77f89e362a2c73ff17b763
Haddad <ahmedabduelrahmen.haddad@esprit.tn>
2021-11-03 17:14:27 +0100

    TimeSheet

---
+++ src/test/java/tn/esprit/spring/DepartmentTests.java
@@ -0,0 +1,15 @@
+package tn.esprit.spring;
+
+import org.junit.Test;
+import tn.esprit.spring.entities.Departement;
+import tn.esprit.spring.entities.Role;
+public class DepartmentTests {
+	Departement E = new Departement();
+ 
+ @Test
+	public void ajouterDepartment() {
+		return;
+    }
+ 
+
+}
---
+++ src/test/java/tn/esprit/spring/TimeSheetApplicationTests.java
@@ -0,0 +1,28 @@
+package tn.esprit.spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import tn.esprit.spring.controller.ControllerEmployeImpl;
+import tn.esprit.spring.entities.Employe;
+import tn.esprit.spring.entities.Role;
+
+
+
+
+@SpringBootTest
+class TimesheetApplicationTests {
+	private static final Logger l = LogManager.getLogger(TimesheetApplicationTests.class.getName());
+
+	
+	@Autowired
+	ControllerEmployeImpl employeControl;
+	
+	@Test
+	void contextLoads() {
+
+	
+	}
+}
